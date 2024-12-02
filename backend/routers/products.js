a8fb434722124ed46cd2391526d7d0f37d25f68f
tuan2804 <tuannguyen12a22k3@gmail.com>
2024-12-02 18:25:59 +0700

    feat(product): add POST endpoint to create a new product

--- backend/routers/products.js
+++ backend/routers/products.js
@@ -2,7 +2,7 @@ const express = require("express");
 const Product = require("../models/Product");
 
 const router = express.Router();
-
+ 
 // Lấy tất cả các sản phẩm theo subcategoryId
 router.get("/:subcategoryId", async (req, res) => {
   try {
